d27ce37179e2b4662fbe998b16ca1a01edc97608
jimi-117 <sho.jimi117@gmail.com>
2026-03-04 11:12:51 +0100

    02: done first backend version

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -14,7 +14,6 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-# core/urls.py
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
@@ -22,8 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # Include the api urls
-    path('', include('api.urls')), 
+    path('', include('api.urls')),
 ]
 
 if settings.DEBUG:
